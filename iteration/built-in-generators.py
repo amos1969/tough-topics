d0f8f0e95f41ce40ef1c9a1096a282779e11c46d
Dave Ames <david.john.ames@gmail.com>
2017-12-19 19:01:01 +0000

    Slightly altered during the session

--- iteration/built-in-generators.py
+++ iteration/built-in-generators.py
@@ -1,11 +1,11 @@
 import itertools
 import time
 
-itr = itertools.count()
-while True:
-    print(next(itr))
-    time.sleep(0.5)
+# itr = itertools.count()
+# while True:
+#     print(next(itr))
+#     time.sleep(0.5)
 
-# itr2 = itertools.product([1, 2, 3], [1, 2, 3])
-# for item in itr2:
-#     print(item)
+itr2 = itertools.product([1, 2, 3], [1, 2, 3])
+for item in itr2:
+    print(item)
