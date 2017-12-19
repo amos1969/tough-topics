dc888db97113b2d48fff512d8cf9ad8866786788
Dave Ames <david.john.ames@gmail.com>
2017-12-19 19:01:13 +0000

    Slightly altered during the session

--- iteration/comprehension.py
+++ iteration/comprehension.py
@@ -1,9 +1,9 @@
 # List comprehension
 squares = [i*i for i in range(10)]
 
-# A list comprehension consists of brackets containing an expression followed by a for clause, then zero or more for
-# or if clauses. The result will be a new list resulting from evaluating the expression in the context of the for and
-# if clauses which follow it.
+# A list comprehension consists of brackets containing an expression followed by a for clause, then zero or more
+# for or if clauses. The result will be a new list resulting from evaluating the expression in the context of
+# the for and if clauses which follow it.
 
 
 print(squares)
